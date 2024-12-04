76da0bb3002c3ee03a0730c2ad9a9716d07065e9
Đào Tiến Đạt <datdao110@gmail.com>
2024-12-04 14:51:31 +0700

    upload app
    app

---
+++ PTPMUDTM/Ql_DATN/UC_DangNhap.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BUS;
+using Ql_DATN;
+
+namespace GUI
+
+{
+    public partial class UC_DangNhap : UserControl
+    {
+        private DangNhapBUS dangNhapBUS = new DangNhapBUS();
+
+        public UC_DangNhap()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            string email = mtxtEmail.Text.Trim();
+            string matKhau = mtxtMK.Text.Trim();
+
+            DangNhapBUS dangNhapBUS = new DangNhapBUS();
+            var (isSuccess, role) = dangNhapBUS.DangNhap(email, matKhau); // Kiểm tra đăng nhập từ BUS
+
+            if (isSuccess)
+            {
+                // Đăng nhập thành công, mở Form1 với vai trò
+                Form1 frmMain = new Form1(role);
+                frmMain.Show();
+                this.FindForm().Hide(); // Ẩn form đăng nhập
+            }
+            else
+            {
+                // Thông báo nếu đăng nhập thất bại
+                MessageBox.Show("Email hoặc mật khẩu không đúng!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
